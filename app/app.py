790652a56c9184450cf02732dbc8267935250583
Matthew holmes <matthew.holmes@nominet.uk>
2021-12-09 16:43:37 +0000

    updated form method and get logic

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os, requests, datetime
 
-
 app = Flask(__name__)
 
 API_KEY = os.getenv('API_KEY')
@@ -10,16 +9,18 @@ API_KEY = os.getenv('API_KEY')
 def index():
 
     response = requests.get("https://api.nasa.gov/planetary/apod?api_key={}".format(API_KEY))
+    
     r = response.json()
     return render_template('index.html', landing_image=r['hdurl'])
     
 
-@app.route('/mars', methods=["GET", "POST"])
+@app.route('/mars', methods=["GET"])
 def mars():
 
     todaysDate = datetime.datetime.now().strftime('%Y-%m-%d')
-    filter = request.form.get('filter', todaysDate)
+    date = request.args.get('date') if request.args.get('date') else todaysDate
+    print(date)
 
-    response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={}&camera=fhaz&api_key={}".format(filter, API_KEY))
+    response = requests.get("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date={}&camera=fhaz&api_key={}".format(date, API_KEY))
     r = response.json()
-    return render_template('mars.html', photos=r['photos'], todaysDate=todaysDate)
+    return render_template('mars.html', photos=r['photos'], todaysDate=todaysDate, date=date)
